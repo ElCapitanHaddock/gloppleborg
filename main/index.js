e3a02a58a2cfdb4628765748bf6578687a961ee2
ElCapitanHaddock <ElCapitanHaddock@users.noreply.github.com>
2023-05-18 20:22:30 -0700

    initial commit

---
+++ main/index.js
@@ -0,0 +1,28 @@
+
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents:
+        [
+    GatewayIntentBits.Guilds,
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers,
+        ]
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.once(Events.MessageCreate, message => {
+    console.log(message)
+});
+
+// Log in to Discord with your client's token
+client.login(token);
